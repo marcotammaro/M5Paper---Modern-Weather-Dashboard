61a94afbcc51d78335cda7401b58302104741525
Marco Tammaro <marcotammaro42@gmail.com>
2023-12-28 17:37:31 +0100

    Better documentation

--- src/Config-sample.h
+++ src/Config-sample.h
@@ -10,7 +10,7 @@
 #define LATITUDE 40.829338
 #define LONGITUDE 14.247718
 #define LOCALIZATION "en"
-#define UNITS "metric"  // metric or imperial
+#define UNITS "metric"
 #define OPENWEATHER_SRV "api.openweathermap.org"
 #define OPENWEATHER_PORT 80
 #define OPENWEATHER_API "YOUR OPEN WEATHER MAP API KEY"
